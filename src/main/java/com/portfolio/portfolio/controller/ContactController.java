3f702b946cc99b401c65bbdd51fcbaa67bed3ee8
Gunderao <gunderaosavalagi@gmail.com>
2026-05-14 19:36:31 +0530

    Updated the controller class to handle the CORS issue

--- src/main/java/com/portfolio/portfolio/controller/ContactController.java
+++ src/main/java/com/portfolio/portfolio/controller/ContactController.java
@@ -1,26 +1,24 @@
 package com.portfolio.portfolio.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.portfolio.portfolio.dto.ContactRequest;
 import com.portfolio.portfolio.service.ContactService;
 
 @RestController
-@RequestMapping("/api/contact")
+@RequestMapping("/api")
 @CrossOrigin(origins = "https://gunderao.netlify.app")
 public class ContactController {
 
-	@Autowired
-	private ContactService contactService;
-	
-	@PostMapping
-	public String sendMessage(@RequestBody ContactRequest request) {
-		contactService.sendEmail(request);
-		return "Email Sent Successfully";
-	}
+    @Autowired
+    private ContactService contactService;
+
+    @PostMapping("/contact")
+    public String sendMessage(@RequestBody ContactRequest request) {
+
+        contactService.sendEmail(request);
+
+        return "Email Sent Successfully";
+    }
 }
